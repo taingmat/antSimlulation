68ef93dd6b66a0f910b261472452ded3d3068960
TIEN HUYNH <tien.huynh5312@gmail.com>
2020-02-06 17:12:43 -0800

    improve chance

--- initAntGrid.py
+++ initAntGrid.py
@@ -1,6 +1,6 @@
 def initAntGrid(n, probAnt):
     import numpy as np
-    site = np.empty((n+2,n+2))
+    site = np.zeros((n+2,n+2))
     direction = [1,2,3,4] # N, E, S, W
     
     site[:,0] = 6 #- Border
@@ -11,7 +11,7 @@ def initAntGrid(n, probAnt):
     for i in range(1, n+1):
         for j in range(1, n+1):
             chance = np.random.rand()
-            if chance > probAnt:
+            if chance < probAnt:
                 site[i,j] = direction[np.random.randint(0,4)]
     
     return site
