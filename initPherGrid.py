196ebba7243e998520c6a821dcbf7a3376935002
TIEN HUYNH <tien.huynh5312@gmail.com>
2020-02-06 17:07:36 -0800

    Fix integer division

--- initPherGrid.py
+++ initPherGrid.py
@@ -7,7 +7,7 @@ def initPherGrid(n, MAXPHER):
     site[0, :] = 0 #- Border
     site[-1,:] = 0 #- Border
     
-    middle_index = n / 2
+    middle_index = int(n / 2)
     for i in range(1,n+1):
         site[middle_index, i] = MAXPHER * i /n
         
