6caaa8e0e55b84c50a5bd3bf366c7246268d7edc
TIEN HUYNH <tien.huynh5312@gmail.com>
2020-02-06 17:04:54 -0800

    Create initPherGrid.py

---
+++ initPherGrid.py
@@ -0,0 +1,14 @@
+def initPherGrid(n, MAXPHER):
+    import numpy as np
+    site = np.empty((n+2),(n+2))
+    
+    site[:,0]  = 0 #- Border
+    site[:,-1] = 0 #- Border
+    site[0, :] = 0 #- Border
+    site[-1,:] = 0 #- Border
+    
+    middle_index = n / 2
+    for i in range(1,n+1):
+        site[middle_index, i] = MAXPHER * i /n
+        
+    return site
